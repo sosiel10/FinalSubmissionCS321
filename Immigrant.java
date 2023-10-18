cc51d269b0d751f348b3cbfb685ff658d2311a9d
BluWolf22 <kevin222vo@gmail.com>
2023-10-18 17:48:53 -0400

    Create Methods For Immigrant and Workflow files
    Created Methods For Immigrant and Workflow files

--- Immigrant.java
+++ Immigrant.java
@@ -1,3 +1,50 @@
 public class Immigrant {
-    
+    private boolean complete;
+    private boolean validAN;
+
+    public Immigrant() {
+    }
+
+    public boolean getComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    public boolean getValidAN() {
+        return validAN;
+    }
+
+    public void setValidAN(boolean validAN) {
+        this.validAN = validAN;
+    }
+
+    public String getName() {
+       
+        return "";
+    }
+
+    public void setName(String name) {
+        
+    }
+
+    public int getAN() {
+       
+        return 0;
+    }
+
+    public void setAN(int an) {
+        
+    }
+
+    public String getRelativeName() {
+        
+        return "";
+    }
+
+    public void setRelativeName(String relativeName) {
+       
+    }
 }
--- Workflow.java
+++ Workflow.java
@@ -1,3 +1,31 @@
 public class Workflow {
+    private String nextStep;
+    private Immigrant form;
 
+    public Workflow() {
+    }
+    public String getNextStep() {
+        return nextStep;
+    }
+
+    public void setNextStep(String nextStep) {
+        this.nextStep = nextStep;
+    }
+
+    public Immigrant getForm() {
+        return form;
+    }
+
+    public void setForm(Immigrant form) {
+        this.form = form;
+    }
+
+    public boolean generateEmail() {
+        
+        return false;
+    }
+
+    public void callNext() {
+        
+    }
 }
