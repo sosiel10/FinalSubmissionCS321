467b16769c0c156a9e5f44871cf3d42b346c0805
Sebastian Villagomez <sebastianoriel123@gmail.com>
2023-11-03 15:23:15 -0400

    Update gui.java

--- gui.java
+++ gui.java
@@ -4,8 +4,10 @@ class gui{
        JFrame frame = new JFrame("My First GUI");
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        frame.setSize(300,300);
-       JButton button1 = new JButton("Button1");
+       JButton button1 = new JButton("Approve Edits");
+       JButton button2 = new JButton("Button 2");
        frame.getContentPane().add(button1); // Adds Button to content pane of frame
+       frame.getContentPane().add(button2);
        frame.setVisible(true);
     }
 }
