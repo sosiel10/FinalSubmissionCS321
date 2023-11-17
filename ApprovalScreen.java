39a23322c759dbda12caf048c3881a2d952c8c71
BluWolf22 <kevin222vo@gmail.com>
2023-11-17 10:24:22 -0500

    Update ApprovalScreen.java

--- ApprovalScreen.java
+++ ApprovalScreen.java
@@ -1,78 +1,65 @@
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+package GroupProject;
 
-public class ApprovalScreen extends Application {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    private Immigrant tempImmigrant; // Temporary Immigrant object for testing
+public class ApprovalScreen extends JFrame {
+	private Workflow workflow;
+	private JLabel nameLabel, aNumberLabel, relativeNameLabel;
+	private JButton approveButton, reviewButton;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        // Temporary Immigrant object
-        tempImmigrant = new Immigrant("John Doe", 123456789, "Jane Doe");
-        tempImmigrant.setComplete(true);
-        tempImmigrant.setValidAN(true);
+	public ApprovalScreen(Workflow workflow) {
+		this.workflow = workflow;
+		GUI();
+	}
 
-        
-        primaryStage.setTitle("Immigration Approval Screen");
+	private void GUI() {
+		setTitle("Approval Screen");
+		setSize(400, 300);
+		setLayout(new GridLayout(5, 1));
 
-        // GridPane for layout
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+		// Create labels
+		nameLabel = new JLabel("Name: " + workflow.getForm().getName());
+		aNumberLabel = new JLabel("Alien Number: " + workflow.getForm().getAN());
+		relativeNameLabel = new JLabel("Relative's Name: " + workflow.getForm().getRelativeName());
+		add(nameLabel);
+		add(aNumberLabel);
+		add(relativeNameLabel);
 
-        // Labels for Immigrant data
-        Label nameLabel = new Label("Name: " + tempImmigrant.getName());
-        Label anLabel = new Label("Alien Number: " + tempImmigrant.getAN());
-        Label relativeNameLabel = new Label("Relative's Name: " + tempImmigrant.getRelativeName());
-        Label statusLabel = new Label("Status: " + (tempImmigrant.getComplete() ? "Complete" : "Incomplete"));
-        Label validANLabel = new Label("Valid AN: " + (tempImmigrant.getValidAN() ? "Yes" : "No"));
-        
-        // Adding labels to grid
-        grid.add(nameLabel, 0, 0);
-        grid.add(anLabel, 0, 1);
-        grid.add(relativeNameLabel, 0, 2);
-        grid.add(statusLabel, 0, 3);
-        grid.add(validANLabel, 0, 4);
+		// Create buttons
+		approveButton = new JButton("Approve");
+		reviewButton = new JButton("Send Back to Review");
+		add(approveButton);
+		add(reviewButton);
 
-        // Approval and send back buttons
-        Button btnApprove = new Button("Approve");
-        Button btnSendBack = new Button("Send Back");
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(btnApprove);
-        hbBtn.getChildren().add(btnSendBack);
-        grid.add(hbBtn, 0, 5);
+		// Add approve button pop up
+		approveButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//boolean emailSent = workflow.generateEmail();
+				JOptionPane.showMessageDialog(ApprovalScreen.this, "Application Approved and Email Sent!");
+				// Close the window
+				ApprovalScreen.this.dispose();
+			}
+		});
 
-        // Button actions
-        btnApprove.setOnAction(e -> handleApprove());
-        btnSendBack.setOnAction(e -> handleSendBack());
+		// Add review button pop up
+		reviewButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Send back to review
+				workflow.setNextStep(0);
+				JOptionPane.showMessageDialog(ApprovalScreen.this, "Sent back for Review");
+			}
+		});
 
-        Scene scene = new Scene(grid, 300, 275);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    private void handleApprove() {
-        // Logic to approve the immigrant data
-        // This would include validation and sending email
-        System.out.println("Approved!");
-    }
-
-    private void handleSendBack() {
-        // Logic to send back the immigrant form for review
-        System.out.println("Sent Back!");
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	//  public static void main(Workflow workflow) {
+	//  new ApprovalScreen(workflow);
+	//}
 }
+
